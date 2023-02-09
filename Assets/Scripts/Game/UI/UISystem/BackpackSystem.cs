120ba2ecf15f3c8159d6f94307e5b0d800588476
syc <shanyuanchen@xd.com>
2023-02-09 21:37:17 +0800

    Update BagPanel.prefab

--- Assets/Scripts/Game/UI/UISystem/BackpackSystem.cs
+++ Assets/Scripts/Game/UI/UISystem/BackpackSystem.cs
@@ -1,7 +1,31 @@
+using System.Collections.Generic;
+using Game.Core;
+using Game.Data;
+using Game.Data.Event;
+using Game.GamePlaySystem.Backpack;
+using Game.UI.ViewData;
+
 namespace Game.UI.UISystem
 {
     public class BackpackSystem : UISystemBase<BackpackSystem>
     {
+        public Dictionary<int, BackpackViewData> GetBackpackData()
+        {
+            var backpackData = new Dictionary<int, BackpackViewData>(3);
+            var data = BackpackManager.Instance.GetBackpack();
+            foreach (var item in data)
+            {
+                var tableItem = ConfigTable.Instance.GetBagItemData(item.Key);
+                backpackData[item.Key] = new BackpackViewData
+                {
+                    count = item.Value,
+                    name = tableItem.Name,
+                    description = tableItem.Content
+                };
+            }
+
+            return backpackData;
+        }
         
     }
 }

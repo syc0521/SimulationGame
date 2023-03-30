6b12a8e7c1e028c063713dda7e1bae1e1cf4b530
syc0521 <65008252+syc0521@users.noreply.github.com>
2023-03-31 01:09:38 +0800

    升级引擎至2022.2.12f1

---
+++ Assets/StreamingAssets/Audio/PC/SoundFX.h
@@ -0,0 +1,26 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : SimGame
+ *  Tool Ver.        : Ver.3.48.02
+ *  ACB  Ver.        : Ver.1.40.0
+ *  File Path        : E:/unity/SimGame/Assets/StreamingAssets/Audio/PC
+ *  File Name        : SoundFX.h
+ *  File Size        : 10336 bytes
+ *  Date Time        : 2023/03/31 00:42:01
+ *  Target           : PC
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : E:/unity/SimGame/Assets/StreamingAssets/Audio/PC/SoundFX.awb
+ *===========================================================================*/
+
+#define CRI_SOUNDFX_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_SOUNDFX_PLACE                ( 0) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+

baa2eff53f59b7c32faf77c5444a20eb39503dc4
syc0521 <65008252+syc0521@users.noreply.github.com>
2023-04-06 00:33:28 +0800

    加入按钮音效

--- Assets/StreamingAssets/Audio/PC/SoundFX.h
+++ Assets/StreamingAssets/Audio/PC/SoundFX.h
@@ -5,20 +5,21 @@
  *  ACB  Ver.        : Ver.1.40.0
  *  File Path        : E:/unity/SimGame/Assets/StreamingAssets/Audio/PC
  *  File Name        : SoundFX.h
- *  File Size        : 10336 bytes
- *  Date Time        : 2023/03/31 00:42:01
+ *  File Size        : 22144 bytes
+ *  Date Time        : 2023/04/06 00:32:47
  *  Target           : PC
- *  Cues             : 1
+ *  Cues             : 2
  *  CueSheet Comment : 
  *  Stream Awb Path  : E:/unity/SimGame/Assets/StreamingAssets/Audio/PC/SoundFX.awb
  *===========================================================================*/
 
-#define CRI_SOUNDFX_CUENUM (1)
+#define CRI_SOUNDFX_CUENUM (2)
 
 /* AISAC Control List (AISAC Control ID) */
 
 /* Cue List (Cue ID) */
 #define CRI_SOUNDFX_PLACE                ( 0) /*  */
+#define CRI_SOUNDFX_BUTTON               ( 1) /*  */
 
 /* Block List (Block Index) */
 
